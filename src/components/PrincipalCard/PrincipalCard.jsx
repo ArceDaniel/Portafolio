59aa1019fdc7a191bada89d590cdbb54d6b1ea19
n1-n2-n3 <xiomaraengine@gmail.com>
2022-11-21 02:58:40 -0300

    carta principal, style y html

--- src/components/PrincipalCard/PrincipalCard.jsx
+++ src/components/PrincipalCard/PrincipalCard.jsx
@@ -1,10 +1,18 @@
-
+import style from './index.module.css'
 
 const PrincipalCard = () =>{
 
     return(
         <>
-        
+        <div className={style.container}>
+            <div className={style.flip-box}>
+                <div className={style.flip-box-inner}>
+                    <div className={style.flip-box-front}>
+                    <div className={style.flip-box-back}></div>
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     )
 
